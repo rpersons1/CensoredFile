2d40abcec8634f61e479b3a75718cecf00f3a057
rpersons1 <rpersons1@mail.csuchico.edu>
2018-03-29 13:11:19 -0700

    wrote a pogram that reads a file

--- main.cpp
+++ main.cpp
@@ -8,11 +8,26 @@ int main()
 {
   ifstream in("example.txt");
   char first = ' ', last = ' ', current;
-
+  
   //Read from the file opened by in, and the first letter (either upper or
   //lower case) should be assigned to first and the last letter (again in
   //either case) from the file to the variable last
 
+  in>>current;
+  while(in.eof())
+  {
+    if(first >= 'a' && last <= 'z' && first >= 'A' && last <= 'B')
+    {
+        if(current == ' ')
+        {
+            first = current;
+        }
+        else
+        {
+            last = current;
+        }
+    }
+  }
 
 
   //Then, finally, this displays the censored version:
